244c9611cff4146b9e2872fef4a1f92d40b83d0b
Nathan Hawkins <nathanlhawkins@gmail.com>
2017-04-18 15:25:59 -0400

    markdown to html with dev link

---
+++ script.js
@@ -0,0 +1,21 @@
+var query = window.location.search;
+if (query) {
+    var branch = query.replace("?", "");
+} else {
+    var branch = "master";
+}
+ 
+// Load readme content
+$.ajax({
+    url: "https://rawgit.com/NLHawkins/URLShortener/master/README.md",
+    dataType: 'text',
+    success: function(data) {
+ 
+        // Convert readme from markdown to html
+        var converter = new Markdown.Converter();
+ 
+        // Show html
+        $(".wrapper").html(converter.makeHtml(data));
+ 
+    }
+});
